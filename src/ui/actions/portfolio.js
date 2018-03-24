6bd5a8d2e58a2e7a7c539f31ed13321811b9e97b
Wuttinan Chaoyos <wuttinan.wch@gmail.com>
2018-03-24 21:44:27 +0700

    update css style and Action

--- src/ui/actions/portfolio.js
+++ src/ui/actions/portfolio.js
@@ -1,10 +1,10 @@
 
 import payload from '../data/data.json'
-import { LOAD_PAGE_SUCCESS } from '../constants/actionTypes'
+import { LOAD_PAGE_REQUEST } from '../constants/actionTypes'
 
 export function loadPagePortfolio() {
     return { 
-      type: LOAD_PAGE_SUCCESS,
+      type: LOAD_PAGE_REQUEST,
       payload
     }
   }
